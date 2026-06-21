ecbfb317ce126f38d69cdbf8a6b1581776920adb
muneeswarann27-tech <muneeswarann27@gmail.com>
2026-06-21 14:07:11 +0530

    project compleated

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -15,7 +15,7 @@ export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
 
-// used to store online users
+
 const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
@@ -24,11 +24,9 @@ io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
   if (userId) userSocketMap[userId] = socket.id;
 
-  // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
-    console.log("A user disconnected", socket.id);
     delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
